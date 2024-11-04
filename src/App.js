d7765130fe4ac5c2bb14f7777c161c5abef54efd
AyoubMarkhouss <ayoub.markhouss@gmail.com>
2024-11-04 10:38:56 +0100

    hehe

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const useTimer = (delay = 1000) => {
+    const [isTimerDone, setIsTimerDone] = useState(false);
+
+    useEffect(() => {
+      // Set a timer to change isTimerDone to true after the specified delay
+      const timer = setTimeout(() => {
+        setIsTimerDone(true);
+      }, delay);
+
+      // Clean up the timer on component unmount
+      return () => clearTimeout(timer);
+    }, [delay]);
+
+    return isTimerDone;
+  };
+  const Timer = useTimer();
+  const notify = () => toast("votre demande à été envoyé avec succès !");
   return (
     <div className="min-h-screen bg-bg py-14">
       <div className="flex justify-center items-center pb-14">
@@ -39,10 +60,51 @@ function App() {
         </div>
       </div>
       <div className="flex items-center justify-center ">
-        <form className="max-w-2xl w-full px-5 md:px-0">
+        <form
+          action="https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8&orgId=00D8d000009q2y7"
+          method="POST"
+          className="max-w-2xl w-full px-5 md:px-0"
+        >
+          <input type="hidden" name="oid" value="00D8d000009q2y7" />
+          {/* <input type="hidden" name="debug" value="1" />
+          <input
+            type="hidden"
+            name="debugEmail"
+            value="ayoub.markhouss@gmail.com"
+          /> */}
+          <input
+            id="lead_source"
+            name="lead_source"
+            type="hidden"
+            value="event_website"
+          />
+          <input
+            id="00N8d00000UVYP7"
+            name="00N8d00000UVYP7"
+            type="hidden"
+            value="1"
+          />
+          <input
+            type="hidden"
+            id="00N8d00000UVYOu"
+            name="00N8d00000UVYOu"
+            value="83"
+            title="Marque d&#39;intérêt"
+          />
+          <input
+            type="hidden"
+            name="retURL"
+            value="https://www.alfaromeo.co.ma"
+          />
+          <br />
+          <br />
           <div className="flex-col flex md:flex-row gap-10 justify-between pb-12 w-full">
             <div className="relative lg:w-72 mb-5 md:mb-0">
               <input
+                id="last_name"
+                maxlength="80"
+                name="last_name"
+                size="20"
                 type="text"
                 className="peer w-full h-10 px-2 pt-4 pb-2 text-white bg-transparent  focus:border-b-2 border-b-2 border-white focus:border focus:border-t-2 focus:border-l-2 focus:border-r-2 focus:border-white focus:outline-none"
                 placeholder=" "
@@ -57,6 +119,10 @@ function App() {
             </div>
             <div className="relative lg:w-72 mb-5 md:mb-0">
               <input
+                id="first_name"
+                maxlength="40"
+                name="first_name"
+                size="20"
                 type="text"
                 className="peer w-full h-10 px-2 pt-4 pb-2 text-white bg-transparent border-b-2  focus:border-b-2 border-white focus:border focus:border-t-2 focus:border-l-2 focus:border-r-2 focus:border-white focus:outline-none"
                 placeholder=" "
@@ -72,15 +138,19 @@ function App() {
           </div>
           <div className="flex-col flex md:flex-row gap-10 justify-between pb-12 w-full">
             <div className="relative lg:w-72 mb-5 md:mb-0">
-              <select className="peer w-full h-10 px-2 pt-2  pb-2 text-white bg-transparent border-b-2 border-white appearance-none focus:border focus:border-t-2 focus:border-l-2 focus:border-r-2 focus:border-b-2 focus:border-white focus:outline-none">
+              <select
+                id="salutation"
+                name="salutation"
+                className="peer w-full h-10 px-2 pt-2  pb-2 text-white bg-transparent border-b-2 border-white appearance-none focus:border focus:border-t-2 focus:border-l-2 focus:border-r-2 focus:border-b-2 focus:border-white focus:outline-none"
+              >
                 <option value="" disabled selected hidden></option>
-                <option value="M." className="semi text-black">
+                <option value="Mr." className="semi text-black">
                   M.
                 </option>
-                <option value="Mme." className="semi text-black">
+                <option value="Ms." className="semi text-black">
                   Mme.
                 </option>
-                <option value="MLLE." className="semi text-black">
+                <option value="Mrs." className="semi text-black">
                   MLLE.
                 </option>
               </select>
@@ -94,6 +164,10 @@ function App() {
             </div>
             <div className="relative lg:w-72 mb-5 md:mb-0">
               <input
+                id="city"
+                maxlength="40"
+                name="city"
+                size="20"
                 type="text"
                 className="peer w-full h-10 px-2 pt-4 pb-2 text-white bg-transparent border-b-2  focus:border-b-2 border-white focus:border focus:border-t-2 focus:border-l-2 focus:border-r-2 focus:border-white focus:outline-none"
                 placeholder=" "
@@ -110,6 +184,10 @@ function App() {
           <div className="flex-col flex md:flex-row gap-10 justify-between pb-12 w-full">
             <div className="relative lg:w-72 mb-5 md:mb-0">
               <input
+                id="email"
+                maxlength="80"
+                name="email"
+                size="20"
                 type="email"
                 className="peer w-full h-10 px-2 pt-4 pb-2 text-white  focus:border-b-2 bg-transparent border-b-2 border-white focus:border focus:border-t-2 focus:border-l-2 focus:border-r-2 focus:border-white focus:outline-none"
                 placeholder=" "
@@ -124,6 +202,10 @@ function App() {
             </div>
             <div className="relative lg:w-72 mb-5 md:mb-0">
               <input
+                id="mobile"
+                maxlength="40"
+                name="mobile"
+                size="20"
                 type="text"
                 className="peer w-full h-10 px-2 pt-4 pb-2 text-white  focus:border-b-2 bg-transparent border-b-2 border-white focus:border focus:border-t-2 focus:border-l-2 focus:border-r-2 focus:border-white focus:outline-none"
                 placeholder=" "
@@ -138,6 +220,17 @@ function App() {
             </div>
           </div>
           <button
+            onClick={(e) => {
+              e.preventDefault(); // Prevents immediate form submission
+
+              if (Timer) {
+                // Check if the timer is done
+                notify(); // Show the toast notification
+                setTimeout(() => {
+                  e.target.closest("form").submit(); // Submit the form after 1 second
+                }, 1000); // 1 second delay before form submission
+              }
+            }}
             type="submit"
             className="semi bg-white w-full h-12 hover:cursor-pointer hover:bg-slate-200"
           >
@@ -145,6 +238,18 @@ function App() {
           </button>
         </form>
       </div>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={900}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
     </div>
   );
 }
